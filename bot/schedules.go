83cff45e956dc0cecff90b3cfe9e9367dfb37f79
JamesCalingo <james.calingo.web@gmail.com>
2024-05-01 22:35:38 -0400

    next function for commuter rail stations

--- bot/schedules.go
+++ bot/schedules.go
@@ -1,10 +1,14 @@
 package bot
 
 import (
+	"encoding/json"
 	"fmt"
+	"gogoat/internal/models"
+	"io"
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
@@ -75,4 +79,79 @@ func getBusSchedules(line string) string {
 		return "This is not a valid bus line."
 	}
 	return fmt.Sprintf("%s\n%s", url, getBusAlerts(line))
+}
+
+func (s *Station) getStationSchedule() ([]models.Prediction, []models.Prediction) {
+	var predictions models.Predictions
+
+	// The API requires a leading zero for minutes < 10, so this formats the minutes to work properly
+	currentMinutes := time.Now().Minute()
+	minutes := ""
+	if currentMinutes < 10 {
+		minutes = "0" + strconv.Itoa(currentMinutes)
+	} else {
+		minutes = strconv.Itoa(currentMinutes)
+	}
+
+	currentTime := fmt.Sprintf("%d:%s", time.Now().Hour(), minutes)
+
+	url := fmt.Sprintf("https://api-v3.mbta.com/schedules?sort=departure_time&page[limit]=10&filter[min_time]=%s&filter[route_type]=2&filter[stop]=%s", currentTime, s.ID)
+
+	res, err := http.Get(url)
+	checkError(err)
+	defer res.Body.Close()
+	data, _ := io.ReadAll(res.Body)
+	json.Unmarshal(data, &predictions)
+
+	var outbound []models.Prediction
+	var inbound []models.Prediction
+	for _, prediction := range predictions.Predictions {
+		if prediction.Attributes.DirectionID == 1 {
+			inbound = append(inbound, prediction)
+		} else if !prediction.Attributes.DepartureTime.IsZero() {
+			outbound = append(outbound, prediction)
+		}
+	}
+	return outbound, inbound
+}
+
+func (s *Station) listStationSchedule() string {
+
+	outbound, inbound := s.getStationSchedule()
+
+	result := func(schedule []models.Prediction) string {
+		output := ""
+		if len(schedule) == 0 {
+			output = "No trains found.\n"
+		}
+		for _, prediction := range schedule {
+			output += fmt.Sprintf("%s\n", prediction.Attributes.DepartureTime.Format(time.Kitchen))
+		}
+		return output
+	}
+	message := fmt.Sprintf("Next trains from %s:\n**OUTBOUND**:\n%s\n**INBOUND**:\n%s", s.Name, result(outbound), result(inbound))
+
+	return message
+}
+
+func (s *Station) predictDirectionCommuter(direction string) string {
+	directions := []string{"outbound", "inbound", "boston"}
+	if !contains(directions, direction) {
+		return "Please use outbound and inbound (or Boston) for commuter trains."
+	}
+
+	outbound, inbound := s.getStationSchedule()
+
+	prediction := outbound[0]
+	if strings.EqualFold(direction, "inbound") || strings.EqualFold(direction, "boston") {
+		prediction = inbound[0]
+	}
+
+	if strings.EqualFold(direction, "boston") {
+		direction = "inbound"
+	}
+
+	output := fmt.Sprintf("The next %s train from %s is at %s", direction, s.Name, prediction.Attributes.DepartureTime.Format(time.Kitchen))
+
+	return output
 }
