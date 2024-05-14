c34f950387a1fb5db6f7375862e84be81b8e279e
JamesCalingo <james.calingo.web@gmail.com>
2024-05-14 18:19:32 -0400

    web subway and commuter versions

---
+++ assets/commuter.js
@@ -0,0 +1,798 @@
+const stations = [ {
+    "name": "South Station Commuter",
+    "alt_name": ["Boston South Station", "Boston South"],
+    "id": "place-south"
+  },
+  {
+    "name": "Back Bay",
+    "alt_name": [],
+    "id": "place-bbsta"
+  },
+  {
+    "name": "Ruggles",
+    "alt_name": [],
+    "id": "place-rugg"
+  },
+  {
+    "name": "Forest Hills",
+    "alt_name": [],
+    "id": "place-forhl"
+  },
+  {
+    "name": "Hyde Park",
+    "alt_name": ["Cleary Square"],
+    "id": "place-NEC-2203"
+  },
+  {
+    "name": "Route 128",
+    "alt_name": ["128", "University"],
+    "id": "place-NEC-2173"
+  },
+  {
+    "name": "Canton Junction",
+    "alt_name": [],
+    "id": "place-NEC-2139"
+  },
+  {
+    "name": "Canton Center",
+    "alt_name": [],
+    "id": "place-SB-0156"
+  },
+  {
+    "name": "Stoughton",
+    "alt_name": [],
+    "id": "place-SB-0189"
+  },
+  {
+    "name": "Sharon",
+    "alt_name": [],
+    "id": "place-NEC-2108"
+  },
+  {
+    "name": "Mansfield",
+    "alt_name": [],
+    "id": "place-NEC-2040"
+  },
+  {
+    "name": "Attleboro",
+    "alt_name": [],
+    "id": "place-NEC-1969"
+  },
+  {
+    "name": "South Attleboro",
+    "alt_name": [],
+    "id": "place-NEC-1969"
+  },
+  {
+    "name": "Pawtucket/Central Falls",
+    "alt_name": ["Pawtucket"],
+    "id": "place-NEC-1891"
+  },
+  {
+    "name": "Providence",
+    "alt_name": [],
+    "id": "place-NEC-1851"
+  },
+  {
+    "name": "TF Green Airport",
+    "alt_name": ["TF Green"],
+    "id": "place-NEC-1768"
+  },
+  {
+    "name": "Wickford Junction",
+    "alt_name": ["Wickford"],
+    "id": "place-NEC-1659"
+  },
+  {
+    "name": "Readville",
+    "alt_name": [],
+    "id": "place-DB-0095"
+  },
+  {
+    "name": "Endicott",
+    "alt_name": [],
+    "id": "place-FB-0109"
+  },
+  {
+    "name": "Dedham Corporate Center",
+    "alt_name": ["Dedham Corp"],
+    "id": "place-FB-0118"
+  },
+  {
+    "name": "Islington",
+    "alt_name": [],
+    "id": "place-FB-0125"
+  },
+  {
+    "name": "Norwood Depot",
+    "alt_name": [],
+    "id": "place-FB-0143"
+  },
+  {
+    "name": "Norwood Central",
+    "alt_name": [],
+    "id": "place-FB-0148"
+  },
+  {
+    "name": "Windsor Gardens",
+    "alt_name": [],
+    "id": "place-FB-0166"
+  },
+  {
+    "name": "Walpole",
+    "alt_name": [],
+    "id": "place-FB-0191"
+  },
+  {
+    "name": "Foxboro",
+    "alt_name": [],
+    "id": "place-FS-0049"
+  },
+  {
+    "name": "Norfolk",
+    "alt_name": [],
+    "id": "place-FB-0230"
+  },
+  {
+    "name": "Franklin",
+    "alt_name": ["Franklin/Dean College"],
+    "id": "place-FB-0275"
+  },
+  {
+    "name": "Forge Park/495",
+    "alt_name": ["Forge Park"],
+    "id": "place-FB-0303"
+  },
+  {
+    "name": "Roslindale Village",
+    "alt_name": ["Roslindale", "Rozzie"],
+    "id": "place-NB-0064"
+  },
+  {
+    "name": "Bellevue",
+    "alt_name": [],
+    "id": "place-NB-0072"
+  },
+  {
+    "name": "Highland",
+    "alt_name": [],
+    "id": "place-NB-0076"
+  },
+  {
+    "name": "West Roxbury",
+    "alt_name": ["West Rox"],
+    "id": "place-NB-0080"
+  },
+  {
+    "name": "Hersey",
+    "alt_name": [],
+    "id": "place-NB-0109"
+  },
+  {
+    "name": "Needham Junction",
+    "alt_name": [],
+    "id": "place-NB-0120"
+  },
+  {
+    "name": "Needham Center",
+    "alt_name": [],
+    "id": "place-NB-0127"
+  },
+  {
+    "name": "Needham Heights",
+    "alt_name": [],
+    "id": "place-NB-0137"
+  },
+  {
+    "name": "Lansdowne",
+    "alt_name": [],
+    "id": "place-WML-0025"
+  },
+  {
+    "name": "Boston Landing",
+    "alt_name": [],
+    "id": "place-WML-0035"
+  },
+  {
+    "name": "Newtonville",
+    "alt_name": [],
+    "id": "place-WML-0081"
+  },
+  {
+    "name": "West Newton",
+    "alt_name": [],
+    "id": "place-WML-0091"
+  },
+  {
+    "name": "Auburndale",
+    "alt_name": [],
+    "id": "place-WML-0102"
+  },
+  {
+    "name": "Wellesley Farms",
+    "alt_name": [],
+    "id": "place-WML-0125"
+  },
+  {
+    "name": "Wellesley Hills",
+    "alt_name": [],
+    "id": "place-WML-0135"
+  },
+  {
+    "name": "Wellesley Square",
+    "alt_name": [],
+    "id": "place-WML-0147"
+  },
+  {
+    "name": "Natick Center",
+    "alt_name": ["Natick"],
+    "id": "place-WML-0177"
+  },
+  {
+    "name": "West Natick",
+    "alt_name": [],
+    "id": "place-WML-0199"
+  },
+  {
+    "name": "Framingham",
+    "alt_name": [],
+    "id": "place-WML-0214"
+  },
+  {
+    "name": "Ashland",
+    "alt_name": [],
+    "id": "place-WML-0252"
+  },
+  {
+    "name": "Southborough",
+    "alt_name": ["Southboro"],
+    "id": "place-WML-0274"
+  },
+  {
+    "name": "Westborough",
+    "alt_name": ["Westboro"],
+    "id": "place-WML-0340"
+  },
+  {
+    "name": "Grafton",
+    "alt_name": [],
+    "id": "place-WML-0364"
+  },
+  {
+    "name": "Worcester",
+    "alt_name": [],
+    "id": "place-WML-0442"
+  },
+  {
+    "name": "Newmarket",
+    "alt_name": [],
+    "id": "place-DB-2265"
+  },
+  {
+    "name": "Uphams Corner",
+    "alt_name": [],
+    "id": "place-DB-2258"
+  },
+  {
+    "name": "Four Corners/Geneva",
+    "alt_name": ["Four Corners"],
+    "id": "place-DB-2249"
+  },
+  {
+    "name": "Talbot Avenue",
+    "alt_name": ["Talbot Ave", "Talbot"],
+    "id": "place-DB-2240"
+  },
+  {
+    "name": "Morton Street",
+    "alt_name": ["Morton St", "Morton"],
+    "id": "place-DB-2230"
+  },
+  {
+    "name": "Blue Hill Avenue",
+    "alt_name": ["Blue Hill Ave"],
+    "id": "place-DB-2222"
+  },
+  {
+    "name": "Fairmount",
+    "alt_name": [],
+    "id": "place-DB-2205"
+  },
+  {
+    "name": "JFK/UMass",
+    "alt_name": ["JFK"],
+    "id": "place-jfk"
+  },
+  {
+    "name": "Quincy Center",
+    "alt_name": [],
+    "id": "place-qnctr"
+  },
+  {
+    "name": "Braintree",
+    "alt_name": [],
+    "id": "place-brntn"
+  },
+  {
+    "name": "Weymouth Landing/East Braintree",
+    "alt_name": ["Weymouth Landing"],
+    "id": "place-GRB-0118"
+  },
+  {
+    "name": "East Weymouth",
+    "alt_name": [],
+    "id": "place-GRB-0146"
+  },
+  {
+    "name": "West Hingham",
+    "alt_name": [],
+    "id": "place-GRB-0162"
+  },
+  {
+    "name": "Nantasket Junction",
+    "alt_name": ["Nantasket"],
+    "id": "place-GRB-0183"
+  },
+  {
+    "name": "Cohasset",
+    "alt_name": [],
+    "id": "place-GRB-0199"
+  },
+  {
+    "name": "North Scituate",
+    "alt_name": [],
+    "id": "place-GRB-0233"
+  },
+  {
+    "name": "Greenbush",
+    "alt_name": [],
+    "id": "place-GRB-0276"
+  },
+  {
+    "name": "South Weymouth",
+    "alt_name": [],
+    "id": "place-PB-0158"
+  },
+  {
+    "name": "Abington",
+    "alt_name": [],
+    "id": "place-PB-0194"
+  },
+  {
+    "name": "Whitman",
+    "alt_name": [],
+    "id": "place-PB-0212"
+  },
+  {
+    "name": "Hanson",
+    "alt_name": [],
+    "id": "place-PB-0245"
+  },
+  {
+    "name": "Halifax",
+    "alt_name": [],
+    "id": "place-PB-0281"
+  },
+  {
+    "name": "Kingston",
+    "alt_name": [],
+    "id": "place-KB-0351"
+  },
+  {
+    "name": "Holbrook/Randolph",
+    "alt_name": [],
+    "id": "place-MM-0150"
+  },
+  {
+    "name": "Montello",
+    "alt_name": [],
+    "id": "place-MM-0186"
+  },
+  {
+    "name": "Brockton",
+    "alt_name": [],
+    "id": "place-MM-0200"
+  },
+  {
+    "name": "Campello",
+    "alt_name": [],
+    "id": "place-MM-0219"
+  },
+  {
+    "name": "Bridgewater",
+    "alt_name": [],
+    "id": "place-MM-0277"
+  },
+  {
+    "name": "Middleborough/Lakeville",
+    "alt_name": ["Middleborough", "Middleboro"],
+    "id": "place-MM-0356"
+  },
+  {
+    "name": "North Station",
+    "alt_name": ["Boston North Station", "Boston North"],
+    "id": "place-north"
+  },
+  {
+    "name": "Chelsea",
+    "alt_name": [],
+    "id": "place-chels"
+  },
+  {
+    "name": "River Works",
+    "alt_name": [],
+    "id": "place-ER-0099"
+  },
+  {
+    "name": "Lynn",
+    "alt_name": ["Lynn Interim"],
+    "id": "place-ER-0117"
+  },
+  {
+    "name": "Swampscott",
+    "alt_name": [],
+    "id": "place-ER-0128"
+  },
+  {
+    "name": "Salem",
+    "alt_name": [],
+    "id": "place-ER-0168"
+  },
+  {
+    "name": "Beverly",
+    "alt_name": [],
+    "id": "place-ER-0183"
+  },
+  {
+    "name": "North Beverly",
+    "alt_name": [],
+    "id": "place-ER-0208"
+  },
+  {
+    "name": "Hamilton/Wenham",
+    "alt_name": [],
+    "id": "place-ER-0227"
+  },
+  {
+    "name": "Ipswich",
+    "alt_name": [],
+    "id": "place-ER-0276"
+  },
+  {
+    "name": "Rowley",
+    "alt_name": [],
+    "id": "place-ER-0312"
+  },
+  {
+    "name": "Newburyport",
+    "alt_name": [],
+    "id": "place-ER-0362"
+  },
+  {
+    "name": "Montserrat",
+    "alt_name": [],
+    "id": "place-GB-0198"
+  },
+  {
+    "name": "Beverly Farms",
+    "alt_name": [],
+    "id": "place-GB-0229"
+  },
+  {
+    "name": "Manchester",
+    "alt_name": ["Manchester-by-the-Sea"],
+    "id": "place-GB-0254"
+  },
+  {
+    "name": "West Gloucester",
+    "alt_name": [],
+    "id": "place-GB-0296"
+  },
+  {
+    "name": "Gloucester",
+    "alt_name": [],
+    "id": "place-GB-0316"
+  },
+  {
+    "name": "Rockport",
+    "alt_name": [],
+    "id": "place-GB-0353"
+  },
+  {
+    "name": "Malden Center",
+    "alt_name": ["Malden"],
+    "id": "place-mlmnl"
+  },
+  {
+    "name": "Oak Grove",
+    "alt_name": [],
+    "id": "place-ogmnl"
+  },
+  {
+    "name": "Wyoming Hill",
+    "alt_name": ["Wyoming"],
+    "id": "place-WR-0062"
+  },
+  {
+    "name": "Melrose/Cedar Park",
+    "alt_name": [],
+    "id": "place-WR-0067"
+  },
+  {
+    "name": "Melrose Highlands",
+    "alt_name": [],
+    "id": "place-WR-0075"
+  },
+  {
+    "name": "Greenwood",
+    "alt_name": [],
+    "id": "place-WR-0085"
+  },
+  {
+    "name": "Wakefield",
+    "alt_name": [],
+    "id": "place-WR-0099"
+  },
+  {
+    "name": "Reading",
+    "alt_name": [],
+    "id": "place-WR-0120"
+  },
+  {
+    "name": "North Wilmington",
+    "alt_name": [],
+    "id": "place-WR-0163"
+  },
+  {
+    "name": "Ballardvale",
+    "alt_name": [],
+    "id": "place-WR-0205"
+  },
+  {
+    "name": "Andover",
+    "alt_name": [],
+    "id": "place-WR-0228"
+  },
+  {
+    "name": "Lawrence",
+    "alt_name": [],
+    "id": "place-WR-0264"
+  },
+  {
+    "name": "Bradford",
+    "alt_name": [],
+    "id": "place-WR-0325"
+  },
+  {
+    "name": "Haverhill",
+    "alt_name": [],
+    "id": "place-WR-0329"
+  },
+  {
+    "name": "West Medford",
+    "alt_name": [],
+    "id": "place-NHRML-0055"
+  },
+  {
+    "name": "Wedgemere",
+    "alt_name": [],
+    "id": "place-NHRML-0073"
+  },
+  {
+    "name": "Anderson/Woburn",
+    "alt_name": ["Anderson", "Anderson RTC", "Woburn"],
+    "id": "place-NHRML-0127"
+  },
+  {
+    "name": "Wilmington",
+    "alt_name": [],
+    "id": "place-NHRML-0152"
+  },
+  {
+    "name": "North Billerica",
+    "alt_name": [],
+    "id": "place-NHRML-0218"
+  },
+  {
+    "name": "Lowell",
+    "alt_name": [],
+    "id": "place-NHRML-0254"
+  },
+  {
+    "name": "Porter",
+    "alt_name": [],
+    "id": "place-portr"
+  },
+  {
+    "name": "Belmont",
+    "alt_name": [],
+    "id": "place-FR-0064"
+  },
+  {
+    "name": "Waverly",
+    "alt_name": [],
+    "id": "place-FR-0074"
+  },
+  {
+    "name": "Waltham",
+    "alt_name": [],
+    "id": "place-FR-0098"
+  },
+  {
+    "name": "Brandeis/Roberts",
+    "alt_name": ["Brandeis"],
+    "id": "place-FR-0115"
+  },
+  {
+    "name": "Kendal Green",
+    "alt_name": [],
+    "id": "place-FR-0132"
+  },
+  {
+    "name": "Lincoln",
+    "alt_name": [],
+    "id": "place-FR-0167"
+  },
+  {
+    "name": "Concord",
+    "alt_name": [],
+    "id": "place-FR-0201"
+  },
+  {
+    "name": "West Concord",
+    "alt_name": [],
+    "id": "place-FR-0219"
+  },
+  {
+    "name": "South Acton",
+    "alt_name": [],
+    "id": "place-FR-0253"
+  },
+  {
+    "name": "Littleton/Route 495",
+    "alt_name": ["Littleton"],
+    "id": "place-FR-0301"
+  },
+  {
+    "name": "Ayer",
+    "alt_name": [],
+    "id": "place-FR-0361"
+  },
+  {
+    "name": "Shirley",
+    "alt_name": [],
+    "id": "place-FR-0394"
+  },
+  {
+    "name": "North Leominster",
+    "alt_name": [],
+    "id": "place-FR-0451"
+  },
+  {
+    "name": "Fitchburg",
+    "alt_name": [],
+    "id": "place-FR-0494"
+  },
+  {
+    "name": "Watchusett",
+    "alt_name": [],
+    "id": "place-FR-3338"
+  },
+  {
+    "name": "Porter Commuter",
+    "alt_name": ["Porter Fitchburg"],
+    "id": "place-portr"
+  },
+  {
+    "name": "JFK/UMass Commuter",
+    "alt_name": ["JFK Commuter"],
+    "id": "place-jfk"
+  },
+  {
+    "name": "Quincy Center Commuter",
+    "alt_name": [],
+    "id": "place-qnctr"
+  },
+  {
+    "name": "Braintree Commuter",
+    "alt_name": [],
+    "id": "place-brntn"
+  },
+  {
+    "name": "Oak Grove Commuter",
+    "alt_name": [""],
+    "id": "place-ogmnl"
+  },
+  {
+    "name": "Malden Center Commuter",
+    "alt_name": ["Malden Commuter"],
+    "id": "place-mlmnl"
+  },
+  {
+    "name": "Back Bay Commuter",
+    "alt_name": ["Back Bay Station Commuter", "Back Bay/South End Commuter"],
+    "id": "place-bbsta"
+  },
+  {
+    "name": "Ruggles Commuter",
+    "alt_name": [],
+    "id": "place-rugg"
+  },
+  {
+    "name": "Forest Hills Commuter",
+    "alt_name": [],
+    "id": "place-forhl"
+  }
+]
+
+
+stations.sort((a, b) => {
+    if (a.name < b.name) return -1
+    return 1
+})
+
+const origin = document.querySelector("#origin")
+
+function createOption(station) {
+    const option = document.createElement("option")
+    option.textContent = station.name ? station.name : station
+    option.value = station.name ? station.name : station
+    return option
+}
+
+stations.forEach(station => {
+    option = createOption(station)
+    origin.appendChild(option)
+})
+
+let start = origin.value
+let directions = ["Outbound", "Inbound"]
+
+function findDestinations(stations, origin) {
+    destination.innerHTML = "<option disabled display=\"none\" selected>Select a station</option>"
+    console.log(origin)
+    const station = stations.find(station => station.name == origin)
+    if (!station) return
+
+    directions.forEach(direction => {
+       
+            option = createOption(direction)
+            destination.appendChild(option)
+        
+    })
+    document.querySelector("#second").hidden = false
+    start = origin
+}
+
+
+function index(array, selected) {
+    document.querySelector("#buttondiv").hidden = false
+    return array.indexOf(selected)
+}
+
+function schedule(start) {
+    const stop = stations.find(station => station.name === start)
+    console.log(start, stop)
+    if (!stop) return
+    let directionID = index(directions, destination.value)
+    let currentTime = new Date().toTimeString().split(" ")[0].slice(0,5)
+    console.log(currentTime)
+    fetch(`https://api-v3.mbta.com/schedules?sort=departure_time&page[limit]=1&filter[min_time]=${currentTime}&filter[stop]=${stop.id}&filter[direction_id]=${directionID}`)
+        .then(res => {
+            res.json()
+                .then(data => {
+                    console.log(data)
+                    let next = data.data[0]
+                    if(next) {
+                    document.querySelector("#info").innerHTML = `Your next train should be arriving arround<br>
+                    <span class="time">${new Date(next.attributes.arrival_time).toLocaleTimeString([], {hour: 'numeric', minute: '2-digit'})}</span>`}
+                    else {
+                        document.querySelector("#info").innerHTML = "No prediction found."
+                    }
+                })
+        })
+        .catch(err => console.error(err))
+}
+
+const button = document.querySelector("button")
+button.addEventListener("click", () => schedule(start))
