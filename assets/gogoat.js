3d4442b1885bc97927f8e71fe157679e9e9fbedc
JamesCalingo <james.calingo.web@gmail.com>
2024-05-15 11:19:56 -0400

    Forgot this

--- assets/gogoat.js
+++ assets/gogoat.js
@@ -62,3 +62,12 @@
 
 // const button = document.querySelector("button")
 // button.addEventListener("click", () => predict(start))
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
